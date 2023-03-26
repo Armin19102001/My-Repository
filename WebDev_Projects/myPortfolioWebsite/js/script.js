9cffe4c3b709e4c6550eeca2a7367f3fdb0e7531
Armin19102001 <128320719+Armin19102001@users.noreply.github.com>
2023-03-27 01:11:42 +0200

    Add files via upload

--- WebDev_Projects/myPortfolioWebsite/js/script.js
+++ WebDev_Projects/myPortfolioWebsite/js/script.js
@@ -62,3 +62,58 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true
 });
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach((counter) => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const c = +counter.innerText;
+
+        const increment = target / 200;
+
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`;
+            setTimeout(updateCounter, 100);
+        }
+    };
+    updateCounter();
+});
+
+
+function hideText() {
+
+    const btn = document.querySelector('#btn');
+    const infoHide = document.querySelector('.main-text');
+
+    infoHide.style.display = "block"
+    btn.style.display = 'none';
+}
+
+
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+
+    var body = 'name: '+name + '<br/> email: ' + email + '<br/> subject: ' +subject + 
+    '<br/> message: '+message;
+
+
+    Email.send({
+        SecureToken : "499d8ff8-0fdc-4ad1-b157-87100eed0e96",
+        To : 'armindjippp@gmail.com',
+        From : "developerarmin01@gmail.com",
+        Subject : "CONTACT MESSAGE",
+        Body : body
+    }).then(
+      message => alert(message)
+    );
+
+});
